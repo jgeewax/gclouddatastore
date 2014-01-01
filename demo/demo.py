a3b8c45029bb4a434cbb539192ed087c05adab81
JJ Geewax <jj@geewax.org>
2014-01-01 17:27:16 -0500

    Factored details out to the top of the file.

--- demo/demo.py
+++ demo/demo.py
@@ -4,11 +4,14 @@ import os.path
 import gclouddatastore
 
 
+DATASET_ID = 'jgatsby-storage'
 CLIENT_EMAIL = '29903432081-2qogb45k5olu0j3b7rebktohkm2ncv2b@developer.gserviceaccount.com'
+PRIVATE_KEY_PATH = os.path.dirname(__file__) + '/jgatsby-storage.key'
+
 
 def main():
   connection = gclouddatastore.get_connection(
-      'jgatsby-storage', CLIENT_EMAIL, os.path.dirname(__file__) + '/jgatsby-storage.key')
+      DATASET_ID, CLIENT_EMAIL, PRIVATE_KEY_PATH)
   query = connection.query('Thing').limit(2)
   results = query.fetch()
   print json.dumps(results, indent=2)
