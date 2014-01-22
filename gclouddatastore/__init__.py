ab3aa1f234119d25e9240461dfa87f15d7410555
JJ Geewax <jj@geewax.org>
2014-01-22 10:28:11 -0500

    Added some more documentation to the top-level package.

--- gclouddatastore/__init__.py
+++ gclouddatastore/__init__.py
@@ -1,12 +1,81 @@
+"""Shortcut methods for getting set up with Google Cloud Datastore.
+
+You'll typically use these to get started with the API:
+
+>>> import gclouddatastore
+>>> dataset = gclouddatastore.get_dataset('dataset-id-here',
+                                          'long-email@googleapis.com',
+                                          '/path/to/private.key')
+>>> # Then do other things...
+>>> query = dataset.query().kind('EntityKind')
+>>> entity = dataset.entity('EntityKind')
+
+The main concepts with this API are:
+
+- :class:`gclouddatastore.connection.Connection`
+  which represents a connection between your machine and the Cloud Datastore
+  API.
+
+- :class:`gclouddatastore.dataset.Dataset`
+  which represents a particular dataset
+  (akin to a database name in relational database world).
+
+- :class:`gclouddatastore.entity.Entity`
+  which represents a single entity in the datastore
+  (akin to a row in relational database world).
+
+- :class:`gclouddatastore.key.Key`
+  which represents a pointer to a particular entity in the datastore
+  (akin to a unique identifier in relational database world).
+
+- :class:`gclouddatastore.query.Query`
+  which represents a lookup or search over the rows in the datastore.
+"""
+
 from connection import Connection
 from credentials import Credentials
 
 
 def get_connection(client_email, private_key_path):
+  """Shortcut method to establish a connection to the Cloud Datastore.
+
+  Use this if you are going to access several datasets
+  with the same set of credentials (unlikely):
+
+  >>> connection = get_connection(email, key_path)
+  >>> dataset1 = connection.dataset('dataset1')
+  >>> dataset2 = connection.dataset('dataset2')
+
+  :type client_email: string
+  :param client_email: The e-mail attached to the service account.
+
+  :type private_key_path: string
+  :param private_key_path: The path to a private key file (this file was
+                           given to you when you created the service
+                           account).
+  """
   credentials = Credentials.get_for_service_account(
       client_email, private_key_path)
   return Connection(credentials=credentials)
 
 def get_dataset(dataset_id, client_email, private_key_path):
+  """Shortcut method to establish a connection to a particular dataset in the Cloud Datastore.
+
+  You'll generally use this as the first call to working with the API.
+
+  :type dataset_id: string
+  :param dataset_id: The id of the dataset you want to use.
+                     This is akin to a database name
+                     and is usually the same as your Cloud Datastore project
+                     name.
+
+  :type client_email: string
+  :param client_email: The e-mail attached to the service account.
+
+  :type private_key_path: string
+  :param private_key_path: The path to a private key file (this file was
+                           given to you when you created the service
+                           account).
+  """
   connection = get_connection(client_email, private_key_path)
   return connection.dataset(dataset_id)
